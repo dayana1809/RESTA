0e3f99b0e06c3bf1066a1fc3bee34df3e2878afa
Usuario <Usuario@B6-603-37795>
2025-03-07 13:40:01 -0500

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTA
+{
+    public class Class1
+    {
+        /// <rest>
+        /// 
+        /// </rest>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static long  Resta (long a, long b)
+        {
+            return (a - b);
+        }
+    }
+}
